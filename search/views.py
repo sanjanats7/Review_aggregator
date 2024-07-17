5b97f5169f3eff14c8a0e39a0dea81b328981aff
Sanjana T S <sanjanats37@gmail.com>
2024-07-17 15:08:54 +0530

    images and cards added for individual movies

--- search/views.py
+++ search/views.py
@@ -1,5 +1,3 @@
-# search/views.py
-
 from django.shortcuts import render
 from .tmdb import search_movies
 
@@ -9,4 +7,11 @@ def search_view(request):
     if query:
         data = search_movies(query)
         results = data.get('results', [])
+        for movie in results:
+            # Constructing full poster path URL
+            if movie.get('poster_path'):
+                movie['poster_url'] = f"https://image.tmdb.org/t/p/w500{movie['poster_path']}"
+            else:
+                # If no poster path is available, you can provide a default image URL or handle it accordingly
+                movie['poster_url'] = "https://via.placeholder.com/500x750?text=No+Image"
     return render(request, 'search.html', {'results': results})
